e8b41c6e41971d4c1b41ed2e7832b5bf63a947c9
Charles Le Losq <charles@Charless-MacBook-Pro.local>
2018-09-18 14:52:10 +1000

    Fixing sudoku function

--- Practicals/Exercise 12 - Sudoku/sudoku_solver.py
+++ Practicals/Exercise 12 - Sudoku/sudoku_solver.py
@@ -1,23 +1,23 @@
+import numpy as np
+
 def sudoku_solver(sudoku):
     changes = True
     while changes:
         changes = False
         for i in range(9):
-            #print("i=",i)
             for j in range(9):
-                #print ("j=",j)
-                if n[i,j]==0:
+                if sudoku[i,j]==0:
                     cand = np.arange(1,10,dtype='int').tolist() 
                     for k in range(0,9):
-                        if n[k,j]==0: continue
+                        if sudoku[k,j]==0: continue
                         try:
-                            cand.remove(n[k,j])
+                            cand.remove(sudoku[k,j])
                         except ValueError:
                             continue
                     for k in range(0,9):
-                        if n[i,k]==0: continue
+                        if sudoku[i,k]==0: continue
                         try:
-                            cand.remove(n[i,k])
+                            cand.remove(sudoku[i,k])
                         except ValueError:
                             continue
                     i3 = 3*int(i/3)
@@ -25,12 +25,12 @@ def sudoku_solver(sudoku):
                     print(i3,j3)
                     for k in range(i3,i3+3):
                         for l in range(j3,j3+3):
-                            if n[k,l]==0: continue
+                            if sudoku[k,l]==0: continue
                             try:
-                                cand.remove(n[k,l])
+                                cand.remove(sudoku[k,l])
                             except ValueError:
                                 continue
                     if len(cand)==1: 
-                        n [i,j] = cand[0]
+                        sudoku[i,j] = cand[0]
                         changes = True
     return sudoku
